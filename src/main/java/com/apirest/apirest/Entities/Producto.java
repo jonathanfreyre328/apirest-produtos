d689c00141ee1a1c290072f122509855defaad4c
puerkahsuciah <jonathanfreyre328@gmail.com>
2024-06-23 22:37:57 -0300

    api farmacia compatible con railway

--- src/main/java/com/apirest/apirest/Entities/Producto.java
+++ src/main/java/com/apirest/apirest/Entities/Producto.java
@@ -8,29 +8,50 @@ import jakarta.persistence.Id;
 @Entity
 public class Producto {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-private String nombre;
-private double precio;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private String categoria;
+    private String imagen;
 
-public Long getId() {
-    return id;
-}
-public void setId(Long id) {
-    this.id = id;
-}
-public String getNombre() {
-    return nombre;
-}
-public void setNombre(String nombre) {
-    this.nombre = nombre;
-}
-public double getPrecio() {
-    return precio;
-}
-public void setPrecio(double precio) {
-    this.precio = precio;
-}
-
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getImagen() {
+        return imagen;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
